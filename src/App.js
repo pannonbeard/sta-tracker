381958e06546b73c2d2da74045ba79a2b8a23804
Phil Hendricks <pfhendri1@gmail.com>
2018-05-12 15:47:29 -0500

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import Layout from './Components/Layout/Layout'
 import CharacterPlayUI from './Containers/Play/Character/CharacterPlayUI'
-
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './Containers/Pages/Home/Home'
+import Characters from './Containers/Pages/Characters/Characters'
+import Campaigns from './Containers/Pages/Campaigns/Campaigns'
+import Account from './Containers/Pages/Account/Account'
 
 import styles from './App.scss';
-
 
 class App extends Component {
   render() {
@@ -13,10 +16,18 @@ class App extends Component {
       <Router>
         <div className={styles.App}>
           <Layout>
-            <Route exact path='/' component={CharacterPlayUI} />
-            <Route path='/characters' render={()=> <div>Characters</div>} />
-            <Route path='/campaigns' render={()=> <div>campaigns</div>} />
-            <Route path='/account' render={()=> <div>account</div>} />
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/characters' component={Characters} />
+              <Route exact path='/characters/new' render={ () => <div>new characters</div> } />
+              <Route path='/characters/:character_id' component={CharacterPlayUI}/>
+              <Route path='/characters/:character_id/play' component={CharacterPlayUI}/>
+              <Route exact path='/campaigns' component={Campaigns} />
+              <Route exact path='/campaigns/new' component={Campaigns} />
+              <Route path='/campaigns/:campaign_id' component={Campaigns} />
+              <Route path='/campaigns/:campaign_id/play' component={Campaigns} />
+              <Route path='/account' component={Account}/>
+            </Switch>
           </Layout>
         </div>
       </Router>
