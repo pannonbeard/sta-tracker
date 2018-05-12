0458efa5f41ed17dc70e18066bec576f7f388b01
Phil Hendricks <pfhendri1@gmail.com>
2018-05-12 09:43:00 -0500

    adjusted data

--- src/Containers/Play/Character/CharacterPlayUI.js
+++ src/Containers/Play/Character/CharacterPlayUI.js
@@ -17,27 +17,42 @@ class CharacterPlayUI extends Component {
   }
 
   render() {
-    const talents = [
-      { name: 'First Talent', description: 'Does something cool' },
-      { name: 'Second Talent', description: 'Does something else cool' },
-      { name: 'Third Talent', description: 'Does something else cool' },
-      { name: 'Fourth Talent', description: 'Does something else cool' },
-    ]
-
-    const weapons = [
-      { name_type: 'Phaser - blaster', action_dice: 3, qualities: 'great'},
-      { name_type: 'Phaser - blaster', action_dice: 3, qualities: 'great'}
-    ]
-
-    const injuries = [
-      { type: 'broken leg', qualities: 'hard to walk moves half as fast'},
-      { type: 'broken leg', qualities: 'hard to walk moves half as fast'},
-    ]
-
-    const equipment = [
-      { name: 'Something something', description: 'Yada yada blah blah'},
-      { name: 'Something something', description: 'Yada yada blah blah'},
-    ]
+    const character = {
+      attributes: [
+        ['Control', 12],
+        ['Fitness', 12],
+        ['Presence', 12],
+        ['Daring', 12],
+        ['Insite', 12],
+        ['Reason', 12],
+      ],
+      disciplines: [
+        ['Command', 12],
+        ['Security', 12],
+        ['Science', 12],
+        ['Conn', 12],
+        ['Engineering', 12],
+        ['Medicine', 12],
+      ],
+      talents: [
+        { name: 'First Talent', description: 'Does something cool' },
+        { name: 'Second Talent', description: 'Does something else cool' },
+        { name: 'Third Talent', description: 'Does something else cool' },
+        { name: 'Fourth Talent', description: 'Does something else cool' },
+      ],
+      weapons: [
+        { name_type: 'Phaser - blaster', action_dice: 3, qualities: 'great'},
+        { name_type: 'Phaser - blaster', action_dice: 3, qualities: 'great'}
+      ],
+      injuries: [
+        { type: 'broken leg', qualities: 'hard to walk moves half as fast'},
+        { type: 'broken leg', qualities: 'hard to walk moves half as fast'},
+      ],
+      equipment: [
+        { name: 'Something something', description: 'Yada yada blah blah'},
+        { name: 'Something something', description: 'Yada yada blah blah'},
+      ]
+    }
 
     const ship = {
       general: {
@@ -79,23 +94,35 @@ class CharacterPlayUI extends Component {
       }
     }
 
-    let screen = <CharacterInfo />
+    let screen = (
+      <CharacterInfo 
+        attributes={character.attributes} 
+        disciplines={character.disciplines}/>
+    )
 
     switch(this.state.currentScreen){
       case 'talents':
-        screen = <CharacterTalents talents={talents}/>
+        screen = <CharacterTalents talents={character.talents}/>
         break
       case 'battle':
-        screen = <CharacterBattle weapons={weapons} injuries={injuries}/>
+        screen = (
+          <CharacterBattle 
+            weapons={character.weapons} 
+            injuries={character.injuries}/>
+        )
         break
       case 'equipment':
-        screen = <CharacterEquipment equipment={equipment}/>
+        screen = <CharacterEquipment equipment={character.equipment}/>
         break
       case 'ship':
         screen = <ShipPlayUI ship={ship}/>
         break
       default:
-        screen = <CharacterInfo />
+        screen =  (
+          <CharacterInfo 
+            attributes={character.attributes} 
+            disciplines={character.disciplines}/>
+        )
     }
 
     return (
