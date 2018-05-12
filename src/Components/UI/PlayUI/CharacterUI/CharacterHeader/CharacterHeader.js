3f1fb0d1983ad75b0c4dc996a7fbce2d3c16eb86
Phil Hendricks <pfhendri1@gmail.com>
2018-05-12 10:03:54 -0500

    changed to import characters

--- src/Components/UI/PlayUI/CharacterUI/CharacterHeader/CharacterHeader.js
+++ src/Components/UI/PlayUI/CharacterUI/CharacterHeader/CharacterHeader.js
@@ -1,38 +1,28 @@
 import React, { Component } from 'react'
 import styles from './CharacterHeader.css'
 
-class CharacterHeader extends Component {
-  state = {
-    character: {
-      name: 'Jim Kirk',
-      player: 'Steve',
-      rank: 'Capitan',
-      ship: 'USS Enterprise',
-      stress: 9,
-      determination: 1
-    }
-  }
-  render() {
-    return (
-      <header className={styles.CharacterHeader}> 
-        <div>
-          <div className={styles.CharacterName}>
-            {this.state.character.name}
-            <span className={styles.PlayerName}>{this.state.character.player}</span>
-          </div>
-          <div className={styles.CharacterStressDetermination}>
-            <div className={styles.CharacterStress}>Stress: <span>{this.state.character.stress}</span></div>
-            <div className={styles.CharacterDetermination}>Determination: <span>{this.state.character.determination}</span></div>
-          </div>
+const characterHeader = (props) => {
+  const shipNameSize = props.character.ship.length > 10 
+  ? { fontSize:  '.85em', paddingTop: '.65em' }
+  : null 
+  return (
+    <header className={styles.CharacterHeader}> 
+      <div>
+        <div className={styles.CharacterName}>
+          {props.character.name}
+          <span className={styles.PlayerName}>{props.character.player}</span>
         </div>
-
-        <div className={styles.CharacterFooter}>
-          <div className={styles.ShipName}>{this.state.character.ship}</div>
-          <div className={styles.CharacterRank}>{this.state.character.rank}</div>
+        <div className={styles.CharacterStressDetermination}>
+          <div className={styles.CharacterStress}>Stress: <span>{props.character.stress}</span></div>
+          <div className={styles.CharacterDetermination}>Determination: <span>{props.character.determination}</span></div>
         </div>
-      </header>
-    )
-  }
+      </div>
+      <div className={styles.CharacterFooter}>
+        <div className={styles.ShipName} style={shipNameSize}>{props.character.ship}</div>
+        <div className={styles.CharacterRank}>{props.character.rank}</div>
+      </div>
+    </header>
+  )
 }
 
-export default CharacterHeader
+export default characterHeader
