4dfb9e9293709848ecc535a76f35868cdbf75dcd
Phil Hendricks <pfhendri1@gmail.com>
2018-05-12 14:28:18 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Layout from './Components/Layout/Layout'
 import CharacterPlayUI from './Containers/Play/Character/CharacterPlayUI'
 
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import styles from './App.scss';
 
@@ -13,7 +13,10 @@ class App extends Component {
       <Router>
         <div className={styles.App}>
           <Layout>
-            <CharacterPlayUI />
+            <Route exact path='/' component={CharacterPlayUI} />
+            <Route path='/characters' render={()=> <div>Characters</div>} />
+            <Route path='/campaigns' render={()=> <div>campaigns</div>} />
+            <Route path='/account' render={()=> <div>account</div>} />
           </Layout>
         </div>
       </Router>
