bf38635bf63f3b241edd28708a3057e7c31d5f66
Phil Hendricks <pfhendri1@gmail.com>
2018-05-13 15:07:26 -0500

    added character contenxt

--- src/App.js
+++ src/App.js
@@ -9,27 +9,47 @@ import NewCharacter from './Containers/Create/Character/NewCharacter'
 import Campaigns from './Containers/Pages/Campaigns/Campaigns'
 import Account from './Containers/Pages/Account/Account'
 
+import { CharacterWrap, CharacterConsumer } from './Contexts/CharacterContext'
+
 import styles from './App.scss';
+
+
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className={styles.App}>
-          <Layout>
-            <Switch>
-              <Route exact path='/' component={Home}/>
-              <Route exact path='/characters' component={Characters} />
-              <Route exact path='/characters/new' component={NewCharacter} />
-              <Route path='/characters/:character_id' component={CharacterPlayUI}/>
-              <Route path='/characters/:character_id/play' component={CharacterPlayUI}/>
-              <Route exact path='/campaigns' component={Campaigns} />
-              <Route exact path='/campaigns/new' component={Campaigns} />
-              <Route path='/campaigns/:campaign_id' component={Campaigns} />
-              <Route path='/campaigns/:campaign_id/play' component={Campaigns} />
-              <Route path='/account' component={Account}/>
-            </Switch>
-          </Layout>
+          <CharacterWrap>
+            <Layout>
+              <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/characters' render={() => (
+                  <CharacterConsumer> 
+                    { ({characters, getCharacters}) => (
+                        <Characters 
+                          characterList={characters} 
+                          fetchCharacters={getCharacters}/> 
+                      ) }
+                  </CharacterConsumer> 
+                )}/>
+                <Route exact path='/characters/new' component={NewCharacter} />
+                <Route path='/characters/:character_id' render={(match) => (
+                  <CharacterConsumer>
+                    {({getCharacter}) => (
+                      <CharacterPlayUI match={match} fetchCharacter={getCharacter}/>
+                    )}
+                  </CharacterConsumer>                    
+                )}/>
+                <Route path='/characters/:character_id/play' component={CharacterPlayUI}/>
+                <Route exact path='/campaigns' component={Campaigns} />
+                <Route exact path='/campaigns/new' component={Campaigns} />
+                <Route path='/campaigns/:campaign_id' component={Campaigns} />
+                <Route path='/campaigns/:campaign_id/play' component={Campaigns} />
+                <Route path='/account' component={Account}/>
+              </Switch>
+            </Layout>
+          </CharacterWrap>
         </div>
       </Router>
     );
