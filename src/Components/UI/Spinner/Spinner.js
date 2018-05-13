36f7da0cdbf1ba927cbf67d0fac644898535cdbc
Phil Hendricks <pfhendri1@gmail.com>
2018-05-13 15:06:40 -0500

    temporary spinner till i make one

---
+++ src/Components/UI/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import symbol from '../../../assets/spinner.svg'
+
+const spinner = () => (
+  <div>
+    <img src={symbol} alt='spinner' />
+  </div>
+)
+
+export default spinner
