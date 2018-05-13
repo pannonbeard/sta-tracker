bd4e3fdfba034e1b8e842b651ef263c407cd99db
Phil Hendricks <pfhendri1@gmail.com>
2018-05-13 17:15:28 -0500

    added character consumer

--- src/Containers/Create/Character/NewCharacter/NewCharacter.js
+++ src/Containers/Create/Character/NewCharacter/NewCharacter.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react'
 
 import CharacterForm from '../CharacterForm/CharacterForm'
 import styles from './NewCharacter.scss'
+import { CharacterConsumer } from '../../../../Contexts/CharacterContext'
 
 class NewCharacter extends Component{
   render(){
     return (
-      <div className={styles.NewCharacter}>
-        <header>
-          <h1><span>New Character</span></h1>
-        </header>
-        <CharacterForm submit={this.props.create}/>
-      </div>
+      <CharacterConsumer>
+        { ({createCharacter}) => (
+          <div className={styles.NewCharacter}>
+          <header>
+            <h1><span>New Character</span></h1>
+          </header>
+          <CharacterForm submit={createCharacter} {...this.props}/>
+        </div>
+        )}
+        
+      </CharacterConsumer>
     )
   }
 }
