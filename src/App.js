ddd64cc89b1d5f393c83b170b49df12848ce5a33
Phil Hendricks <pfhendri1@gmail.com>
2018-05-13 22:10:37 -0500

    added user login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { auth, provider } from './firebase'
 
 import Layout from './Components/Layout/Layout'
 import CharacterPlayUI from './Containers/Play/Character/CharacterPlayUI'
@@ -10,33 +11,75 @@ import EditCharacter from './Containers/Create/Character/EditCharacter/EditChara
 import Campaigns from './Containers/Pages/Campaigns/Campaigns'
 import Account from './Containers/Pages/Account/Account'
 import { CharacterWrap } from './Contexts/CharacterContext'
+import { UserProvider, UserConsumer } from './Contexts/UserContext'
+import Auth from './Containers/Auth/Auth'
+import Logout from './Containers/Auth/logout'
 
 import styles from './App.scss';
 
 class App extends Component {
+  state = {
+    user: null,
+    logIn: () => {
+      auth.signInWithPopup(provider)
+        .then((result) => {
+          const user = result.user;
+          this.setState({
+            user
+          });
+        });
+    },
+    logOut: () => {
+      auth.signOut()
+      .then(() => {
+        this.setState({
+          user: null
+        });
+      });
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      } 
+    });
+  }
+
   render() {
     return (
-      <CharacterWrap>
-        <Router>
-          <div className={styles.App}>
-            <Layout>
-              <Switch>
-                <Route exact path='/' component={Home}/>
-                <Route exact path='/characters' component={Characters}/>
-                <Route exact path='/characters/new' component={NewCharacter} />
-                <Route path='/characters/:character_id/edit' component={EditCharacter} />
-                <Route path='/characters/:character_id' component={CharacterPlayUI} />
-                <Route path='/characters/:character_id/play' component={CharacterPlayUI}/>
-                <Route exact path='/campaigns' component={Campaigns} />
-                <Route exact path='/campaigns/new' component={Campaigns} />
-                <Route path='/campaigns/:campaign_id' component={Campaigns} />
-                <Route path='/campaigns/:campaign_id/play' component={Campaigns} />
-                <Route path='/account' component={Account}/>
-              </Switch>
-            </Layout>
-          </div>
-        </Router>
-      </CharacterWrap>
+      <UserProvider value={this.state}>
+        <CharacterWrap>
+          { this.state.user 
+          ? <Router>
+              <div className={styles.App}>
+                <Layout>
+                  <Switch>
+                    <Route exact path='/' component={Home}/>
+                    <Route exact path='/characters' component={Characters}/>
+                    <Route exact path='/characters/new' component={NewCharacter} />
+                    <Route path='/characters/:character_id/edit' component={EditCharacter} />
+                    <Route path='/characters/:character_id' component={CharacterPlayUI} />
+                    <Route path='/characters/:character_id/play' component={CharacterPlayUI}/>
+                    <Route exact path='/campaigns' component={Campaigns} />
+                    <Route exact path='/campaigns/new' component={Campaigns} />
+                    <Route path='/campaigns/:campaign_id' component={Campaigns} />
+                    <Route path='/campaigns/:campaign_id/play' component={Campaigns} />
+                    <Route path='/account' component={Account}/>
+                    <Route path='/logout' component={Logout} />
+                  </Switch>
+                </Layout>
+              </div>
+            </Router> 
+          : <UserConsumer>
+              { ({logIn}) => (
+                <Auth signIn={logIn}/>
+              )}
+            </UserConsumer>
+          }
+        </CharacterWrap>
+      </UserProvider>
     );
   }
 }
